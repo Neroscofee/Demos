6b80745160325afc817f76e32339aeeae27ce1a2
Neroscofee <604119562@qq.com>
2018-02-06 00:33:59 +0800

    Revert "Revert "1""
    This reverts commit 0466f26c81ff8fa8dc78cb8a1f69d68e7f009995.

--- Demos/AppDelegate.m
+++ Demos/AppDelegate.m
@@ -49,8 +49,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"App启动时调用此方法!");
     // Override point for customization after application launch.
-//    ViewController *vc = [[ViewController alloc] init];//表视图
+    ViewController *vc = [[ViewController alloc] init];//表视图
     
 //    TestViewController *vc = [[TestViewController alloc] init];
 //    self.window.rootViewController = vc;
@@ -74,7 +75,7 @@
 //        SnowViewController *vc = [[SnowViewController alloc] init];//下雪动画
 //    RecipeController *vc = [[RecipeController alloc] init];//用plist当Model源
 //    FireViewController *vc = [[FireViewController alloc] init];//烟花
-    DynamicMenuViewController *vc = [[DynamicMenuViewController alloc] init];
+//    DynamicMenuViewController *vc = [[DynamicMenuViewController alloc] init];
     
     
     UINavigationController *navi = [[UINavigationController alloc] initWithRootViewController:vc];
@@ -115,24 +116,29 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
+    NSLog(@"应用从活动状态进入到非活动状态时调用此方法!");
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
+    NSLog(@"应用进入后台时调用此方法!");
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
+    NSLog(@"应用进入到前台但还未处于活动状态时调用此方法!");
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
+    NSLog(@"应用进入前台并处于活动状态时调用此方法!");
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
+    NSLog(@"应用被杀死时调用此方法!");
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
